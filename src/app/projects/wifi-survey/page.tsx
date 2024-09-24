aebe18ab03bb416731b1cd3784363544adc5ed2d
shima <shimanoity@gmail.com>
2024-09-24 23:19:44 +0200

    Initial commit

---
+++ src/app/projects/wifi-survey/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WiFiSurveyPage: React.FC = () => {
+  return (
+    <div>
+      <h1>WiFi Survey Heatmap</h1>
+      <p>Hier kannst du die WiFi-Signalstärke visualisieren.</p>
+    </div>
+  );
+};
+
+export default WiFiSurveyPage;
